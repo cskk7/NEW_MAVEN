45b0894f4f0f4d323e14e59af61d846728c75734
PrasannaS-1998 <prasannaavip@gmail.com>
2024-01-31 18:20:49 +0530

    updated pom

--- src/test/java/ORANGE/ORANGE_1.feature
+++ src/test/java/ORANGE/ORANGE_1.feature
@@ -8,3 +8,8 @@ Feature: Orange
     When user provide his valid username
     And click the reset password button
     Then site should redirect user to "send password to mail" page
+
+#  Scenario:
+#    Given user navigate to login page
+#    When user provide valid username
+#    And user provid valid password
--- src/test/java/ORANGE/TestRunner_Orange.java
+++ src/test/java/ORANGE/TestRunner_Orange.java
@@ -4,6 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/ORANGE/ORANGE_1.feature",glue = "ORANGE",
-        plugin = {"pretty", "teamcity","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestRunner_Orange extends AbstractTestNGCucumberTests {
 }
